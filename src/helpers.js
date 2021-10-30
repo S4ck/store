b791e3e8f3186342209db36786c320bdf0b374fb
JcaveroR <jcaveror@gmail.com>
2021-10-30 12:03:04 -0500

    Update helpers.js
    cambio de idioma y moneda

--- src/helpers.js
+++ src/helpers.js
@@ -104,13 +104,13 @@ export function getWspUrl(orderData) {
     });
   }
 
-  const WSP_URL = `https://api.whatsapp.com/send/?phone=${N}&text=%2A${"Order"}%3A%2A%20${ID}%0A%0A%2A${"Client"}%3A%2A%20${name}%0A%0A%2A${"Phone"}%3A%2A%20${phone}%0A%0A%2A${
-    withDelivery ? "Address" + "%3A%2A%20" + address + " %0A%0A%2A" : ""
-  }${withDelivery ? "City" + "%3A%2A%20" + city + "%0A%0A%2A" : ""}${
-    withDelivery ? "Schedule" + "%3A%2A%20" + schedule + "%0A%0A%2A" : ""
-  }${comment ? "Comment" + "%3A%2A%20" + comment + "%0A%0A%2A" : ""}${"Items List"}%3A%2A${cartListforUrl}%0A%0A%2A${
-    withDelivery ? "Sub Total" + "%3A%2A%20$" + subTotal + " %0A%0A%2A" : ""
-  }${withDelivery ? "Delivery Fee" + "%3A%2A%20$" + shippingCost + " %0A%0A%2A" : ""}S/{"Total"}%3A%2A%20${total}%0A%0A`;
+  const WSP_URL = `https://api.whatsapp.com/send/?phone=${N}&text=%2A${"Orden"}%3A%2A%20${ID}%0A%0A%2A${"Cliente"}%3A%2A%20${name}%0A%0A%2A${"Celular"}%3A%2A%20${phone}%0A%0A%2A${
+    withDelivery ? "Dirección" + "%3A%2A%20" + address + " %0A%0A%2A" : ""
+  }${withDelivery ? "Ciudad" + "%3A%2A%20" + city + "%0A%0A%2A" : ""}${
+    withDelivery ? "Horario" + "%3A%2A%20" + schedule + "%0A%0A%2A" : ""
+  }${comment ? "Commentario" + "%3A%2A%20" + comment + "%0A%0A%2A" : ""}${"Mi Pedido"}%3A%2A${cartListforUrl}%0A%0A%2A${
+    withDelivery ? "Sub Total" + "%3A%2A%20S/" + subTotal + " %0A%0A%2A" : ""
+  }${withDelivery ? "Delivery Fee" + "%3A%2A%20S/" + shippingCost + " %0A%0A%2A" : ""}${"Total"}%3A%2A%20S/{total}%0A%0A`;
 
   return WSP_URL;
 }
