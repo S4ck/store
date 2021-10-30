bd1d56a025bec788b1e85b84bc988136ecf69443
JcaveroR <jcaveror@gmail.com>
2021-10-30 11:56:15 -0500

    Update helpers.js
    cambio idioma y S/ total

--- src/helpers.js
+++ src/helpers.js
@@ -10,19 +10,19 @@ export const getFormValidations = () => {
     name: {
       required: {
         value: true,
-        message: "Name is required",
+        message: "Tu Nombre es Requerido",
       },
       maxLength: {
         value: 20,
-        message: "Max Length 20 chars",
+        message: "Max 20 Caracteres",
       },
       minLength: {
         value: 5,
-        message: "Min Length 5 chars",
+        message: "Min 5 Caracteres",
       },
       pattern: {
         value: /^[A-Za-z ]{5,20}$/,
-        message: "Icorrect Name",
+        message: "Nombre Incorrecto",
       },
     },
 
@@ -30,59 +30,59 @@ export const getFormValidations = () => {
     phone: {
       required: {
         value: true,
-        message: "Phone is required",
+        message: "Tu Teléfono es Requerido",
       },
       maxLength: {
-        value: 20,
-        message: "Max Length 20 chars",
+        value: 10,
+        message: "Max 10 Caracteres",
       },
       minLength: {
-        value: 5,
-        message: "Min Length 5 chars",
+        value: 9,
+        message: "Min 9 Caracteres",
       },
       pattern: {
-        value: /^[0-9]{5,20}$/,
-        message: "Icorrect Phone",
+        value: /^[0-9]{9,10}$/,
+        message: "Teléfono Incorrecto",
       },
     },
     //address
     address: {
       required: {
         value: true,
-        message: "Address is required",
+        message: "Dirección Requerida",
       },
       maxLength: {
         value: 20,
-        message: "Max Length 20 chars",
+        message: "Max 20 caracteres",
       },
       minLength: {
         value: 5,
-        message: "Min Length 5 chars",
+        message: "Min 5 caracteres",
       },
       pattern: {
         value: /^[0-9a-zA-Z ]{5,20}$/,
-        message: "Icorrect Address",
+        message: "Dirección Incorrecta",
       },
     },
     //city
     city: {
       required: {
         value: true,
-        message: "City is required",
+        message: "Ciudad Requerida",
       },
     },
     //schedule
     schedule: {
       required: {
         value: true,
-        message: "Schedule is required",
+        message: "Horario Requerido",
       },
     },
     //extra comment
     comment: {
       maxLength: {
         value: 25,
-        message: "Max Length 25 chars",
+        message: "Max 25 caracteres",
       },
     },
   };
@@ -110,7 +110,7 @@ export function getWspUrl(orderData) {
     withDelivery ? "Schedule" + "%3A%2A%20" + schedule + "%0A%0A%2A" : ""
   }${comment ? "Comment" + "%3A%2A%20" + comment + "%0A%0A%2A" : ""}${"Items List"}%3A%2A${cartListforUrl}%0A%0A%2A${
     withDelivery ? "Sub Total" + "%3A%2A%20$" + subTotal + " %0A%0A%2A" : ""
-  }${withDelivery ? "Delivery Fee" + "%3A%2A%20$" + shippingCost + " %0A%0A%2A" : ""}${"Total"}%3A%2A%20${total}%0A%0A`;
+  }${withDelivery ? "Delivery Fee" + "%3A%2A%20$" + shippingCost + " %0A%0A%2A" : ""}S/{"Total"}%3A%2A%20${total}%0A%0A`;
 
   return WSP_URL;
 }
