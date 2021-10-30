3f8142b48631b5b40cd52fee6e5329fe2e8e142b
JcaveroR <jcaveror@gmail.com>
2021-10-29 22:12:09 -0500

    Update Cart.jsx
    shopping cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = ({ showCart = false, setCart }) => {
       <DrawerContent p="4">
         <Flex justify="space-between" align="center" w="100%">
           <Heading as="h2" size="xl">
-            Shopping Cart
+            Tu Pedido está casi listo...
           </Heading>
           <DrawerCloseButton size="lg" position="static" />
         </Flex>
